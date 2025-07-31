5e0c2d76fe2aa1ae98394ade3ef2c2a0ff40db21
koszegig <gabor.koszegi69@gmail.com>
2025-07-31 14:55:23 +0200

    első

---
+++ Logosztics/src/main/java/hu/cubix/koszegig/logosztics/logosztics/LogoszticsApplication.java
@@ -0,0 +1,13 @@
+package hu.cubix.koszegig.logosztics.logosztics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogoszticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogoszticsApplication.class, args);
+    }
+
+}
---
+++ Logosztics/src/test/java/hu/cubix/koszegig/logosztics/logosztics/LogoszticsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.cubix.koszegig.logosztics.logosztics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogoszticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
